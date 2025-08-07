27e7a95cf8f6125bcd481de74ba94b8f2239c77a
devhunting <221843820+devhunting@users.noreply.github.com>
2025-08-07 16:57:55 +0400

    Update hellospeaker.js

--- hellospeaker.js
+++ hellospeaker.js
@@ -1,5 +1,12 @@
 var helloSpeaker = {};
 
-helloSpeaker.speak = function (name) {
-  console.log("Hello " + name);
+helloSpeaker.speak = function(name) {
+  var message = "Hello " + name;
+  console.log(message);
+
+  var greetingsDiv = document.getElementById("greetings");
+  var p = document.createElement("p");
+  p.textContent = message;
+  p.className = "hello";
+  greetingsDiv.appendChild(p);
 };
